e1ffb51038eaf3fa12991d29739c9a14beddaad2
rafacostavilla <rafaelcostavilla@gmail.com>
2024-09-15 12:42:14 +1200

    Create a default summarize_author, and change the default implementation for summarize to call that. Implement the new method for NewsArticle and Tweet and experiment use the summarize method after all

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 pub trait Summary {
+
+    fn summarize_author(&self) -> String;
+
     fn summarize(&self) -> String {
-        String::from("(Read more...)")
+        format!("(Read more from {}...)", self.summarize_author())
     }
 }
 
@@ -12,6 +15,9 @@ pub struct NewsArticle {
 }
 
 impl Summary for NewsArticle{
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
     // fn summarize(&self) -> String {
     //     format!("{}, by {} ({})", self.headline, self.author, self.location)
     // }
@@ -25,6 +31,10 @@ pub struct Tweet{
 }
 
 impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
     }
